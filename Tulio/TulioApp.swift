f97af2e2c0a59ec3d42d4da733c8c473d9415c3f
Anna Chen <59426102+theannachen@users.noreply.github.com>
2022-10-22 20:54:00 -0500

    Add files via upload

---
+++ Tulio/TulioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TulioApp.swift
+//  Tulio
+//
+//  Created by Anna Chen on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TulioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
